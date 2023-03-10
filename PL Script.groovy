d970ee2286e861e05b4f61a185f8b3f6c2802a92
nxt-hj <39750826+nxt-hj@users.noreply.github.com>
2023-03-10 16:59:50 +0800

    Update PL Script.groovy

--- PL Script.groovy
+++ PL Script.groovy
@@ -2,7 +2,7 @@ node {
     def IP
     stage('同步代码') {
         // Get some code from a GitHub repository
-        checkout([$class: 'GitSCM', branches: [[name: 'refs/heads/$branchName']], extensions: [[$class: 'PruneStaleBranch']], userRemoteConfigs: [[credentialsId: '0a03e51e-969b-41a2-9393-630055229bfc', url: 'http://gitlab.eegrid.com/nxt-pc/efos3.0.git']]])
+        checkout([$class: 'GitSCM', branches: [[name: 'refs/heads/$branchName']], extensions: [[$class: 'PruneStaleBranch']], userRemoteConfigs: [[credentialsId: '0a03e51e-969b-41a2-9393-63005529bfc', url: 'http://xx.com/xx/xx.git']]])
 
         echo params.branchName
     }
@@ -26,7 +26,7 @@ node {
     stage('同步静态资源'){
         echo IP
         withEnv(["IP=$IP"]) {
-            sh 'rsync -avrz src/platform/$platform/static/_@server_resource/ root@${IP}:/var/www/oms_huangjian/web_$platform/_@server_resource/'
+            sh 'rsync -avrz src/platform/$platform/static/xxx/ root@${IP}:/var/www/xxx/web_$platform/xx/'
         }
     }
     stage('构建打包') {
@@ -42,7 +42,7 @@ node {
                         sshTransfer(cleanRemote: false, excludes: '', execCommand: 
                         '''#! /bin/bash
                             shopt -s extglob 
-                            cd /var/www/oms_huangjian/web_$platform 
+                            cd /var/www/xxx/web_$platform 
                             rm -rf !(_@server_resource)''', 
                             execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: 'dist/'),
                         sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/var/www/oms_huangjian/web_$platform', remoteDirectorySDF: false, removePrefix: 'dist/0.1.1', sourceFiles: 'dist/0.1.1/**')
